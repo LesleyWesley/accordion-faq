e623a0f60eb0915b8ce3935290dbd3b36326181b
lesley_w <lrwesley92@gmail.com>
2021-06-14 21:04:23 -0500

    Finished adding mobile styling.  Started adding javascript variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Selects the orange arrow
+const arrow = document.querySelector(".question i");
+
+// Selects the question heading
+
+const question = document.querySelector(".question h2");
+
+//Selects answer div
+
+const answer = document.querySelector (".answer");
