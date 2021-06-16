e6df6201584b99b40508e3ed85008ad915d9994e
lesley_w <lrwesley92@gmail.com>
2021-06-16 12:18:52 -0500

    Worked on accordion faq functionability

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 // Selects the orange arrow
 const arrow = document.querySelector(".question i");
 
-// Selects the question heading
 
-const question = document.querySelector(".question h2");
+
+//Selects the question div
+
+const questionDiv = document.querySelectorAll(".question");
 
 //Selects answer div
 
 const answer = document.querySelector (".answer");
+
+//====================================================
+
+// Adds click event for questions
+
+questionDiv.forEach(question) => {
+  question.onclick = function () {
+    this.classList.toggle("hide");
+    let content = this.nextElementSibling;
+
+    if (content.classList.includes("hide")) {
+      content.classList.remove("hide");
+    } else {
+      content.classList.add("hide");
+    }
+  }
+};
