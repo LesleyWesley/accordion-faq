8f9323bed00f23f9c744952278f4d04b507e1006
lesley_w <lrwesley92@gmail.com>
2021-06-21 17:19:16 -0500

    Made suggested updates to javascript and css

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 const qAndA = document.querySelectorAll(".question-and-answer");
 
 for (eachQAndA of qAndA) {
-  const questionDiv = eachQAndA.querySelector(".question");
+  const questionDiv = eachQAndA.querySelector("button");
   const question = eachQAndA.querySelector("h2");
   const arrow = eachQAndA.querySelector(".question i");
   const answer = eachQAndA.querySelector(".answer");
   console.log(arrow);
 
-  question.addEventListener("click", function () {
+  questionDiv.addEventListener("click", function () {
     if (answer.classList.contains("hide")) {
       answer.classList.remove("hide");
-      question.style.fontWeight = "700";
-      question.style.color = "#1D1E35";
-      arrow.style.transform = 'rotate(0deg)';
+      question.classList.add("active");
+      arrow.classList.add("active");
     } else {
       answer.classList.add("hide");
-      question.style.fontWeight = "400";
-      question.style.color = "#4A4B5E";
-      arrow.style.transform = 'rotate(180deg)';
+      question.classList.remove("active");
+      arrow.classList.remove("active");
     }
   });
 };
